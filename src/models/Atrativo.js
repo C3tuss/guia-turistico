f3cb34efc75875672d6338b9cf07ae569837a84b
C3tuss <alexandrevilela.sds@gmail.com>
2024-07-20 15:38:02 -0300

    Criação das classes Destinos e Atrativos

--- src/models/Atrativo.js
+++ src/models/Atrativo.js
@@ -11,6 +11,25 @@ const atrativoSchema = new mongoose.Schema({
   dicas: String
 });
 
+class Atrativo {
+  constructor(destinoId, nome, tipo, descricao, dicas) {
+    this.destinoId = destinoId;
+    this.nome = nome;
+    this.tipo = tipo;
+    this.descricao = descricao;
+    this.dicas = dicas;
+  }
+
+  static async getByDestinoId(destinoId) {
+    return await AtrativoModel.find({ destinoId });
+  }
+
+  async save() {
+    const atrativo = new AtrativoModel(this);
+    return await atrativo.save();
+  }
+}
+
 const Atrativo = mongoose.model('Atrativo', atrativoSchema);
 
 module.exports = Atrativo;
--- src/models/Destino.js
+++ src/models/Destino.js
@@ -10,6 +10,29 @@ const destinoSchema = new mongoose.Schema({
   }
 });
 
+class Destino {
+  constructor(nome, descricao, imagem, localizacao) {
+    this.nome = nome;
+    this.descricao = descricao;
+    this.imagem = imagem;
+    this.localizacao = localizacao;
+  }
+
+  static async getAll() {
+    return await DestinoModel.find();
+  }
+
+  static async getById(id) {
+    return await DestinoModel.findById(id);
+  }
+
+  async save() {
+    const destino = new DestinoModel(this);
+    return await destino.save();
+  }
+}
+
+
 const Destino = mongoose.model('Destino', destinoSchema);
 
 module.exports = Destino;
