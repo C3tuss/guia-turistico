73c43048903d50150bf60254f6d5946ebda4d627
C3tuss <alexandrevilela.sds@gmail.com>
2024-08-03 11:07:51 -0300

    estilização da página e correção de um bug ao carregar o mapa

--- DESAFIO 5/guia-turistico-main/run.js
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import app from "./src/app.js";
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log("Servidor esta rodando na porta 3000.");
-    console.log("Acesse aqui http://localhost:3000/");
-});
