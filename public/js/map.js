c5fa98b8c186286e6214818625ca4db9fd108b3d
C3tuss <alexandrevilela.sds@gmail.com>
2024-08-05 11:33:48 -0300

    link com a api do back

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,15 @@
+const backendUrl = 'https://guia-turistico-pw4m.onrender.com';
+
 let map;
-let markers = []; // Array para armazenar os marcadores
+let markers = [];
 
 function initMap() {
-  // Inicializa o mapa centrado no Maranhão
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -2.529, lng: -44.3028 }, // Coordenadas de São Luís, Maranhão
+    center: { lat: -2.529, lng: -44.3028 },
     zoom: 7,
   });
 
-  // Carregar os destinos e adicionar marcadores
-  fetch('/api/destinos')
+  fetch(`${backendUrl}/api/destinos`)
     .then(response => response.json())
     .then(destinos => {
       destinos.forEach(destino => {
@@ -25,23 +25,9 @@ function initMap() {
 
         marker.addListener('click', () => {
           infoWindow.open(map, marker);
-
-          // Carregar atrativos para o destino clicado
-          fetch(`/api/atrativos/${destino._id}`)
-            .then(response => response.json())
-            .then(atrativos => {
-              let atrativosContent = '<h4>Atrações</h4><ul>';
-              atrativos.forEach(atrativo => {
-                atrativosContent += `<li><b>${atrativo.nome}</b> (${atrativo.tipo}): ${atrativo.descricao} <br> <i>Dicas:</i> ${atrativo.dicas}</li>`;
-              });
-              atrativosContent += '</ul>';
-              infoWindow.setContent(`<h3>${destino.nome}</h3><p>${destino.descricao}</p><img src="${destino.imagem}" alt="${destino.nome}" style="width:100px;height:auto;">${atrativosContent}`);
-              infoWindow.open(map, marker);
-            })
-            .catch(error => console.error('Erro ao carregar atrativos:', error));
         });
 
-        markers.push(marker); // Adiciona o marcador ao array
+        markers.push(marker);
       });
     })
     .catch(error => console.error('Erro ao carregar destinos:', error));
@@ -49,40 +35,22 @@ function initMap() {
 
 function buscarDestinos() {
   const nome = document.getElementById('destinoNome').value.trim();
-  console.log('Buscando destinos com nome:', nome);
+  if (!nome) return;
 
-  if (!nome) {
-    console.log('Nome do destino não fornecido.');
-    return;
-  }
-
-  fetch(`/api/buscarDestinos?nome=${encodeURIComponent(nome)}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Erro na resposta da API');
-      }
-      return response.json();
-    })
+  fetch(`${backendUrl}/api/buscarDestinos?nome=${encodeURIComponent(nome)}`)
+    .then(response => response.json())
     .then(destinos => {
-      console.log('Destinos recebidos:', destinos);
-
-      if (!Array.isArray(destinos)) {
-        throw new Error('A resposta da API não é um array');
-      }
-
-      // Limpa a lista de resultados da busca
+      clearMarkers();
       clearSearchResults();
 
       if (destinos.length > 0) {
         const resultadoBusca = document.getElementById('resultadoBusca');
-
         destinos.forEach(destino => {
           const listItem = document.createElement('li');
           listItem.textContent = destino.nome;
           listItem.onclick = () => {
-            // Centraliza o mapa no destino clicado
             map.setCenter(destino.localizacao);
-            map.setZoom(10); // Ajuste o nível de zoom conforme necessário
+            map.setZoom(10);
           };
 
           resultadoBusca.appendChild(listItem);
@@ -108,16 +76,12 @@ function buscarDestinos() {
     .catch(error => console.error('Erro ao buscar destinos:', error));
 }
 
-
-
 function clearMarkers() {
-  // Remove todos os marcadores do mapa e limpa a lista
   markers.forEach(marker => marker.setMap(null));
-  markers = []; // Limpa o array de marcadores
+  markers = [];
 }
 
 function clearSearchResults() {
-  // Limpa a lista de resultados da busca
   const resultadoBusca = document.getElementById('resultadoBusca');
   resultadoBusca.innerHTML = '';
 }
